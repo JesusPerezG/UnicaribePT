c0886abce0f137e7d207031f7228ba6279139dfc
JesusPerezG <jesusp010794@gmail.com>
2016-06-19 17:18:45 -0500

    layout

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+   unicaribe: {
+     adapter: 'sails-mongo',
+     host: 'localhost',
+     port: 27017,
+     user: 'test', //optional
+     //password: 'password', //optional
+     database: 'UnicaribePT' //optional
+   },
 
   /***************************************************************************
   *                                                                          *
